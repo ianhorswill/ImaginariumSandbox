1441c829afff2dcb649b6068b4e93c735b6b541b
Ian Horswill <ian@northwestern.edu>
2026-03-27 21:20:01 -0500

    Initial commit

---
+++ Imaginarium.js
@@ -0,0 +1,40 @@
+﻿{
+
+const COMMA = {
+    scope: 'keyword',
+    begin: ','
+};
+
+const PERIOD = {
+    scope: 'keyword',
+    begin: '\\.'
+};
+
+hljs.registerLanguage('imaginarium', function () {
+    return {
+        keywords: {
+            keyword: ['and', 'or', 'not',
+                'can', 'must', 'should',
+                'are', 'is', 'be', 'implies',
+                'exist',
+                'kind', 'way',
+                'of', 'from', 'at', 'between', 'with',
+                'a', 'A', 'An', 'an',
+                '.',
+                'has', 'have',
+                'mutually', 'exclusive',
+                'other',
+                'all', 'every', 'most', 'least', 'any',
+                'rare', 'common',
+                'always', 'itself',
+                'plural', 'singular',
+                'identified', 'described',
+                'mention', 'print', 
+                'called', 'its',
+                'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+            ]
+        },
+        contains: [ COMMA, PERIOD ]
+    }
+})
+}
